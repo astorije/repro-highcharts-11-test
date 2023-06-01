00a4502b74c8de45f7ba761a67d15d99ff7e1d91
Jérémie Astori <jeremie@astori.fr>
2023-06-01 12:34:24 -0400

    Set up a basic chart
    Signed-off-by: Jérémie Astori <jeremie@astori.fr>

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const options = {
+  chart: {
+    type: "spline",
+  },
+  title: {
+    text: "My chart",
+  },
+  series: [
+    {
+      data: [1, 2, 1, 4, 3, 6],
+    },
+  ],
+};
+
+const App = () => (
+  <div>
+    <HighchartsReact highcharts={Highcharts} options={options} />
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
